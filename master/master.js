ad439c77148d50206dd101a909e148e52e9a1d78
John Brzustowski <jbrzusto@fastmail.fm>
2017-04-03 17:57:14 +0000

    change path to find_tags_unifile to reflect placement of find_tags in own project directory (they are from separate repos).

--- master/master.js
+++ master/master.js
@@ -71,8 +71,8 @@ Deployment = new (require("./deployment.js").Deployment) (
 
 Deployment.shortLabel = Deployment.shortLabel.replace(/-/g,"_");
 
-TagFinder     = new (require('./tagfinder.js').TagFinder) (TheMatron, 
-                                                           "/home/pi/proj/sensorgnome/find_tags/find_tags_unifile", 
+TagFinder     = new (require('./tagfinder.js').TagFinder) (TheMatron,
+                                                           "/home/pi/proj/find_tags/find_tags_unifile",
                                                            TheMatron.tagDBFile,
                                                            Deployment.module_options.find_tags.params
                                                           );
